13ca3e13c482c4d33ecb8fbebb507637ec455282
hinerm <hinerm@gmail.com>
2023-09-14 11:29:48 -0500

    Add debug flag to mvn ci

--- .github/build.sh
+++ .github/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
 mvn -Djavadoc.skip -pl scijava/scijava-taglets clean install
+export BUILD_ARGS="-X"
 sh ci-build.sh
